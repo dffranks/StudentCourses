554c5215adc9128e7a4bc7bc6a3b46a9ae37d7ef
Daniel Franks <delta.foxtrot129@gmail.com>
2015-04-13 12:23:00 -0400

    Courses.txt added.

--- courses.php
+++ courses.php
@@ -14,18 +14,19 @@ if (isset($_SESSION['user'])) {
 
 	$usrID = $_SESSION['user'];
 
-	studData($usrID);
+	grabStudData($usrID);
+	grabCourseData();
 	$fname;
 	$lname;
 	$major;
 
-	$studData = studData($usrID);
-	if (!isset($studData)) {
+	$grabStudData = grabStudData($usrID);
+	if (!isset($grabStudData)) {
 		echo "<p>An error has occured while fetching data.</p>";
 	} else {
-		$fname = $studData['fname'];
-		$lname = $studData['lname'];
-		$major = $studData['major'];
+		$fname = $grabStudData['fname'];
+		$lname = $grabStudData['lname'];
+		$major = $grabStudData['major'];
 
 		print<<<INFO
 			<h2>ID #$usrID <br />
@@ -45,7 +46,7 @@ INFO;
 /****** FUNCTIONS ******/
 /***********************/
 
-function studData($id) {
+function grabStudData($id) {
 	$ID = $id;
 
 	# Open the file
@@ -73,7 +74,52 @@ function studData($id) {
 	fclose($handle);
 }
 
+function grabCourseData() {
+	$courses = array();
+	$file = 'courses.txt';
+	$handle = fopen($file, 'r');
+	$content = file_get_contents($file);
+	$dataArr = explode(';', $content);
+	foreach($dataArr as $k1=>$arr1Val) {
+		echo "Arr1: $k1 => $arr1Val</br>";
+		$dataItems = explode(',', $arr1Val);
+		foreach ($dataItems as $k2=>$arr2Val) {
+		//	$courses array( array($k1))
+		// FUCK
+
+
+			echo "Arr2: $k2 => $arr2Val</br>";
+		}
+		
+	//	$dataOut[]
+	}
+}
+
 ?>
+
+<table align="center">
+	<tr>
+		<th>✓</th>
+		<th>ID</th>
+		<th>Name</th>
+		<th>Max Students</th>
+		<th>Currently Enrolled</th>
+	</tr>
+	<tr>
+		<td><input type="checkbox" name="hist101"></td>
+		<td>HIST101</td>
+		<td>World History I</td>
+		<td>30</td>
+		<td>22</td>
+	</tr>
+	<tr>
+		<th><input type="checkbox" name="math304"></th>
+		<td>MATH304</td>
+		<td>Advanced Calculus</td>
+		<td>20</td>
+		<td>17</td>
+	</tr>
+</table>
 
 </div> <!--end wrapper-->
 
